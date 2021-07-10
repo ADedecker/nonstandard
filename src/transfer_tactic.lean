4af31a8c4a0e7cdbda96863078f05a58f6e2b813
Anatole Dedecker <anatolededecker@gmail.com>
2021-07-11 00:52:53 +0200

    Restructure

--- src/transfer_tactic.lean
+++ src/transfer_tactic.lean
@@ -7,70 +7,106 @@ open tactic expr
 
 namespace tactic
 
-meta def transfer_step_one_aux (tgt : expr) (new_vars : list expr) : tactic (list expr) :=
+section step_one
+
+private meta def forall_rule (l : expr) (α : expr) : tactic unit :=
+do  `(filter %%ι) ← infer_type l,
+    e ← to_expr ``(filter.germ.forall_iff_forall_lift_pred %%l),
+    rewrite_target e
+
+private meta def exists_rule (l : expr) (α : expr) : tactic unit :=
+do  `(filter %%ι) ← infer_type l,
+    e ← to_expr ``(filter.germ.exists_iff_exists_lift_pred %%l),
+    rewrite_target e
+
+meta def transfer_step_one_aux (tgt : expr) : tactic unit :=
 do  `(%%lhs ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 1)",
     match rhs with
-    | `(∀ _ : (filter.germ %%l %%α), _) := 
-      do  `(filter %%ι) ← infer_type l,
-          e ← to_expr ``(filter.germ.forall_iff_forall_lift_pred %%l),
-          rewrite_target e,
-          return new_vars
-    | `(∃ _ : (filter.germ %%l %%α), _) := 
-      do  `(filter %%ι) ← infer_type l,
-          e ← to_expr ``(filter.germ.exists_iff_exists_lift_pred %%l),
-          rewrite_target e,
-          return new_vars
+    | `(∀ _ : (filter.germ %%l %%α), _) := forall_rule l α
+    | `(∀ _ : ℝ*, _) := forall_rule `(filter.hyperfilter ℕ : filter ℕ) `(ℝ)
+    | `(∃ _ : (filter.germ %%l %%α), _) := exists_rule l α
+    | `(∃ _ : ℝ*, _) := exists_rule `(filter.hyperfilter ℕ : filter ℕ) `(ℝ)
     | _ := fail "No known pattern applicable (step 1)"
     end
 
-meta def transfer_step_two_aux (tgt : expr) (new_vars : list expr) : tactic (list expr) :=
+end step_one
+
+section step_two
+
+meta def transfer_step_two_aux (tgt : expr) : tactic unit :=
 do  `(%%lhs ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 2)",
     match lhs with
     | `(∀ _, _) := 
       do  some (name, _, _) ← get_binder none tt lhs,
           name ← get_unused_name name,
           refine ``(forall_congr _),
-          new_var ← intro name,
-          return (new_var :: new_vars)
+          intro name,
+          skip
     | _ := fail "No known pattern applicable (step 2)"
     end
 
-meta def transfer_step_three_aux (tgt : expr) (new_vars : list expr) : tactic (list expr) :=
+end step_two
+
+section step_three
+
+meta def transfer_step_three_aux (tgt : expr) : tactic unit :=
 do  `(filter.germ.lift_pred %%p %%x ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 3)",
     match p with
-    | `(λ _, ∀ y, %%q) := 
-      do  e ← to_expr ``(filter.germ.lift_pred_forall_iff_forall_lift_pred'),
-          rewrite_target e,
-          return new_vars
+    | `(λ _, %%q) := 
+      do  (i, r) ← open_lambdas q,
+          local_context >>= trace,
+          trace i,
+          trace r,
+          match r with
+          | `(∀ _ : %%tp, %%s) := 
+            do trace tp, t' ← infer_type tp,
+            (do t' ← infer_type tp,
+                trace t',
+                unify t' `(Prop),
+                e ← to_expr ``(filter.germ.lift_pred_imp_iff_imp_lift_pred),
+                trace e,
+                skip) <|>
+            (do e ← to_expr ``(filter.germ.lift_pred_forall_iff_forall_lift_pred'),
+                rewrite_target e)
+          | `(∃ _ : %%tp, %%s) := 
+            (do unify tp `(Prop), 
+                e ← to_expr ``(filter.germ.lift_pred_exists_prop_iff_and_lift_pred'),
+                e' ← to_expr ``(exists_prop),
+                rewrite_target e,
+                rewrite_target e') <|> 
+            (do e ← to_expr ``(filter.germ.lift_pred_exists_iff_exists_lift_pred'),
+                rewrite_target e)
+          | _ := fail "No known pattern applicable (step 3)"
+          end
     | _ := fail "No known pattern applicable (step 3)"
     end
 
-meta def transfer_step_four_aux (tgt : expr) (new_vars : list expr) : tactic unit :=
-do  new_vars.mmap' (λ x, do { refine ``((%%x).induction_on _), name ← get_unused_name, intro name }),
-    reflexivity
+end step_three
+
+meta def transfer_step_four_aux (tgt : expr) : tactic unit :=
+do  hyps ← local_context,
+    hyps.mmap' (λ x, try $ 
+      do  `(filter.germ _ _) ← infer_type x,
+          refine ``((%%x).induction_on _), 
+          name ← get_unused_name, 
+          intro name ),
+    try (reflexivity)
 
 namespace interactive
 
 setup_tactic_parser
 
 meta def transfer_step_one : tactic unit :=
-do  tgt ← target,
-    transfer_step_one_aux tgt [],
-    skip
+target >>= transfer_step_one_aux
 
 meta def transfer_step_two : tactic unit :=
-do  tgt ← target,
-    transfer_step_two_aux tgt [],
-    skip
+target >>= transfer_step_two_aux
 
 meta def transfer_step_three : tactic unit :=
-do  tgt ← target,
-    transfer_step_three_aux tgt [],
-    skip
+target >>= transfer_step_three_aux
 
 meta def transfer_step_four : tactic unit :=
-do  tgt ← target,
-    transfer_step_four_aux tgt []
+target >>= transfer_step_four_aux
 
 end interactive
 
@@ -92,6 +128,19 @@ begin
   transfer_step_three,
   transfer_step_two,
   transfer_step_four,
+  rw filter.germ.coe_eq,
+  refl
+end
+
+open filter
+
+example (l : ℝ) (u : ℕ → ℝ) :
+  (∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, abs (u n - l) < ε) ↔
+  (∀ ε > 0, ∃ N : (hyperfilter ℕ : filter ℕ).germ ℕ, ∀ n ≥ N, germ.map abs (germ.map u n - ↑l) < ε) :=
+begin
+  transfer_step_one,
+  transfer_step_two,
+  transfer_step_three,
 end
 
 end tactic
