f58907feb2d81d67a525adc14e5ab8ecae434d49
Anatole Dedecker <anatolededecker@gmail.com>
2021-07-11 22:27:48 +0200

    Add rules

--- src/transfer_tactic.lean
+++ src/transfer_tactic.lean
@@ -36,12 +36,24 @@ section step_two
 meta def transfer_step_two_aux (tgt : expr) : tactic unit :=
 do  `(%%lhs ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 2)",
     match lhs with
-    | `(∀ _, _) := 
-      do  some (name, _, _) ← get_binder none tt lhs,
+    | `(∀ _ : %%t, _) := 
+      (do t' ← infer_type t,
+          unify t' `(Prop),
+          refine ``(imp_congr _ _)) <|>
+      (do some (name, _, _) ← get_binder none tt lhs,
           name ← get_unused_name name,
           refine ``(forall_congr _),
           intro name,
+          skip)
+    | `(∃ _ : %%t, _) := 
+      do  --some (name, _, _) ← get_binder none tt lhs, TODO
+          name ← get_unused_name,
+          refine ``(exists_congr _),
+          intro name,
           skip
+    | `(_ ∧ _) := 
+      do  name ← get_unused_name,
+          refine ``(and_congr _ _)
     | _ := fail "No known pattern applicable (step 2)"
     end
 
@@ -52,32 +64,24 @@ section step_three
 meta def transfer_step_three_aux (tgt : expr) : tactic unit :=
 do  `(filter.germ.lift_pred %%p %%x ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 3)",
     match p with
-    | `(λ _, %%q) := 
-      do  (i, r) ← open_lambdas q,
-          local_context >>= trace,
-          trace i,
-          trace r,
-          match r with
-          | `(∀ _ : %%tp, %%s) := 
-            do trace tp, t' ← infer_type tp,
-            (do t' ← infer_type tp,
-                trace t',
-                unify t' `(Prop),
-                e ← to_expr ``(filter.germ.lift_pred_imp_iff_imp_lift_pred),
-                trace e,
-                skip) <|>
-            (do e ← to_expr ``(filter.germ.lift_pred_forall_iff_forall_lift_pred'),
-                rewrite_target e)
-          | `(∃ _ : %%tp, %%s) := 
-            (do unify tp `(Prop), 
-                e ← to_expr ``(filter.germ.lift_pred_exists_prop_iff_and_lift_pred'),
-                e' ← to_expr ``(exists_prop),
-                rewrite_target e,
-                rewrite_target e') <|> 
-            (do e ← to_expr ``(filter.germ.lift_pred_exists_iff_exists_lift_pred'),
-                rewrite_target e)
-          | _ := fail "No known pattern applicable (step 3)"
-          end
+    | `(λ _, ∀ y, %%q) := 
+      (do e ← to_expr ``(filter.germ.lift_pred_forall_iff_forall_lift_pred'),
+          rewrite_target e) <|>
+      (do e ← to_expr ``(filter.germ.lift_pred_imp_iff_imp_lift_pred),
+          rewrite_target e)
+    | `(λ _, ∃ y, %%q) := 
+      (do e ← to_expr ``(filter.germ.lift_pred_exists_iff_exists_lift_pred'),
+          rewrite_target e) <|>
+      (do e ← to_expr ``(filter.germ.lift_pred_exists_prop_iff_and_lift_pred),
+          e' ← to_expr ``(exists_prop),
+          rewrite_target e,
+          rewrite_target e')
+    | `(λ _, _ < _) := 
+      do  e ← to_expr ``(filter.germ.lift_pred_lt_iff_lt_map),
+          rewrite_target e
+    | `(λ _, _ > _) := 
+      do  e ← to_expr ``(filter.germ.lift_pred_lt_iff_lt_map),
+          rewrite_target e
     | _ := fail "No known pattern applicable (step 3)"
     end
 
@@ -135,12 +139,27 @@ end
 open filter
 
 example (l : ℝ) (u : ℕ → ℝ) :
-  (∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, abs (u n - l) < ε) ↔
-  (∀ ε > 0, ∃ N : (hyperfilter ℕ : filter ℕ).germ ℕ, ∀ n ≥ N, germ.map abs (germ.map u n - ↑l) < ε) :=
+  (∀ ε > 0, ∃ N ≥ (1 : ℕ), ∀ n ≥ N, abs (u n - l) < ε) ↔
+  (∀ ε > 0, ∃ N ≥ (1 : (hyperfilter ℕ : filter ℕ).germ ℕ), ∀ n ≥ N, germ.map abs (germ.map u n - ↑l) < ε) :=
 begin
   transfer_step_one,
   transfer_step_two,
   transfer_step_three,
+  transfer_step_two,
+  { transfer_step_three, 
+    transfer_step_four },
+  { transfer_step_three,
+    transfer_step_two,
+    transfer_step_three,
+    transfer_step_two,
+    { transfer_step_four },
+    { transfer_step_three,
+      transfer_step_two,
+      transfer_step_three,
+      transfer_step_two,
+      { transfer_step_four },
+      { transfer_step_three,
+        transfer_step_four } } }
 end
 
 end tactic
