509a2c8d3384e3093c3122162428eee7ebbb1528
Anatole Dedecker <anatolededecker@gmail.com>
2021-07-12 23:20:37 +0200

    Full tactic !

--- src/transfer_tactic.lean
+++ src/transfer_tactic.lean
@@ -19,7 +19,7 @@ do  `(filter %%ι) ← infer_type l,
     e ← to_expr ``(filter.germ.exists_iff_exists_lift_pred %%l),
     rewrite_target e
 
-meta def transfer.lift_lhs (tgt : expr) : tactic unit :=
+meta def transfer_lift_lhs (tgt : expr) : tactic unit :=
 do  `(%%lhs ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 1)",
     match rhs with
     | `(∀ _ : (filter.germ %%l %%α), _) := forall_rule l α
@@ -33,7 +33,7 @@ end lift_lhs
 
 section congr
 
-meta def transfer.congr (tgt : expr) : tactic unit :=
+meta def transfer_congr (tgt : expr) : tactic unit :=
 do  `(%%lhs ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 2)",
     match lhs with
     | `(∀ _ : %%t, _) := 
@@ -61,7 +61,7 @@ end congr
 
 section push_lift
 
-meta def transfer.push_lift (tgt : expr) : tactic unit :=
+meta def transfer_push_lift (tgt : expr) : tactic unit :=
 do  `(filter.germ.lift_pred %%p %%x ↔ %%rhs) ← (return tgt) | fail "Goal is not an equivalence (step 3)",
     match p with
     | `(λ _, ∀ y, %%q) := 
@@ -89,18 +89,15 @@ end push_lift
 
 section induction
 
-meta def transfer.induction (tgt : expr) : tactic unit :=
+meta def transfer_induction (tgt : expr) : tactic unit :=
 local_context >>= list.mmap' (λ x, try $ 
   do  `(filter.germ _ _) ← infer_type x,
       refine ``((%%x).induction_on _), 
       name ← get_unused_name, 
       intro name )
 
-meta def transfer.close (tgt : expr) : tactic unit :=
-transfer.induction tgt >> reflexivity
-
-meta def transfer.simp (tgt : expr) : tactic unit :=
-transfer.induction tgt >> `[simp]
+meta def transfer_close (tgt : expr) : tactic unit :=
+transfer_induction tgt >> reflexivity
 
 end induction
 
@@ -108,67 +105,53 @@ namespace interactive
 
 setup_tactic_parser
 
-meta def transfer.lift_lhs : tactic unit :=
-target >>= tactic.transfer.lift_lhs
+meta def transfer_lift_lhs : tactic unit :=
+target >>= tactic.transfer_lift_lhs
 
-meta def transfer.congr : tactic unit :=
-target >>= tactic.transfer.congr
+meta def transfer_congr : tactic unit :=
+target >>= tactic.transfer_congr
 
-meta def transfer.push_lift : tactic unit :=
-target >>= tactic.transfer.push_lift
+meta def transfer_push_lift : tactic unit :=
+target >>= tactic.transfer_push_lift
 
-meta def transfer.induction : tactic unit :=
-target >>= tactic.transfer.induction
+meta def transfer_induction : tactic unit :=
+target >>= tactic.transfer_induction
 
-meta def transfer.simp : tactic unit :=
-target >>= tactic.transfer.simp
+meta def transfer_close : tactic unit :=
+target >>= tactic.transfer_close
 
-meta def transfer.close : tactic unit :=
-target >>= tactic.transfer.close
+meta def transfer_step : tactic unit :=
+transfer_close <|>
+transfer_congr <|>
+(transfer_push_lift >> try transfer_congr) <|>
+(transfer_lift_lhs >> try transfer_congr)
 
-meta def transfer.step : tactic unit :=
-transfer.close <|>
-transfer.congr <|>
-(transfer.push_lift >> try transfer.congr) <|>
-(transfer.lift_lhs >> try transfer.congr) <|>
-transfer.simp -- WARNING
+meta def transfer : tactic unit :=
+focus (repeat transfer_step)
 
 end interactive
 
 example (α ι : Type*) [preorder α] (l : ultrafilter ι) (a : α) : 
   (∀ x, a ≤ x) ↔ (∀ x : (l : filter ι).germ α, ↑a ≤ x) :=
 begin
-  transfer.step,
-  transfer.step
+  transfer
 end
 
 example (α ι : Type*) [preorder α] (l : ultrafilter ι) (a : α) : 
   (∀ x y : α, x = y) ↔ (∀ x y : (l : filter ι).germ α, x = y) :=
 begin
-  transfer.step,
-  transfer.step,
-  transfer.step,
-  transfer.step
+  transfer,
+  transfer_induction,
+  simp,
+  refl
 end
 
 open filter
 
+set_option profiler true
 example (l : ℝ) (u : ℕ → ℝ) :
   (∀ ε > 0, ∃ N ≥ (1 : ℕ), ∀ n ≥ N, abs (u n - l) < ε) ↔
   (∀ ε > 0, ∃ N ≥ (1 : (hyperfilter ℕ : filter ℕ).germ ℕ), ∀ n ≥ N, germ.map abs (germ.map u n - ↑l) < ε) :=
-begin
-  transfer.step,
-  transfer.step,
-  { transfer.step, 
-    transfer.step },
-  { transfer.step, 
-    transfer.step, 
-    { transfer.step },
-    { transfer.step, 
-      transfer.step,
-      { transfer.step },
-      { transfer.step, 
-        transfer.step } } }
-end
+by transfer
 
 end tactic
